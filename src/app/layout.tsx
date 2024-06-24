86d8c3752369bd1496187c9cbe3b0b5830188b85
wellyton-xs <wellyton.offcer@gmail.com>
2024-06-24 11:17:40 -0300

    adicionando speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import Navbar from "src/components/navbar";
 import { JetBrains_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const jbm = JetBrains_Mono({ weight: "700", subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Analytics />
+          <SpeedInsights />
         </div>
       </body>
     </html>
